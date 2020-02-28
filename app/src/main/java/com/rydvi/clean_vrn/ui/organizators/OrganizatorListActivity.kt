29e8a401bb612dc93c6d74c47d764ade15c9bc4d
RyDVi <dmitrii.kardan.rch@mail.ru>
2020-02-28 09:47:35 +0300

    added master-detail navigation for organizators screens

--- app/src/main/java/com/rydvi/clean_vrn/ui/organizators/OrganizatorListActivity.kt
+++ app/src/main/java/com/rydvi/clean_vrn/ui/organizators/OrganizatorListActivity.kt
@@ -76,62 +76,6 @@ class OrganizatorListActivity : AppCompatActivity() {
         }
 
     private fun setupRecyclerView(recyclerView: RecyclerView) {
-        recyclerView.adapter = SimpleItemRecyclerViewAdapter(this, DummyContent.ITEMS, twoPane)
-    }
-
-    class SimpleItemRecyclerViewAdapter(
-        private val parentActivity: OrganizatorListActivity,
-        private val values: List<DummyContent.DummyItem>,
-        private val twoPane: Boolean
-    ) :
-        RecyclerView.Adapter<SimpleItemRecyclerViewAdapter.ViewHolder>() {
-
-        private val onClickListener: View.OnClickListener
-
-        init {
-            onClickListener = View.OnClickListener { v ->
-                val item = v.tag as DummyContent.DummyItem
-                if (twoPane) {
-                    val fragment = OrganizatorDetailFragment().apply {
-                        arguments = Bundle().apply {
-                            putString(OrganizatorDetailFragment.ARG_ITEM_ID, item.id)
-                        }
-                    }
-                    parentActivity.supportFragmentManager
-                        .beginTransaction()
-                        .replace(R.id.organizator_detail_container, fragment)
-                        .commit()
-                } else {
-                    val intent = Intent(v.context, OrganizatorDetailActivity::class.java).apply {
-                        putExtra(OrganizatorDetailFragment.ARG_ITEM_ID, item.id)
-                    }
-                    v.context.startActivity(intent)
-                }
-            }
-        }
-
-        override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
-            val view = LayoutInflater.from(parent.context)
-                .inflate(R.layout.organizator_list_content, parent, false)
-            return ViewHolder(view)
-        }
-
-        override fun onBindViewHolder(holder: ViewHolder, position: Int) {
-            val item = values[position]
-            holder.idView.text = item.id
-            holder.contentView.text = item.content
-
-            with(holder.itemView) {
-                tag = item
-                setOnClickListener(onClickListener)
-            }
-        }
-
-        override fun getItemCount() = values.size
-
-        inner class ViewHolder(view: View) : RecyclerView.ViewHolder(view) {
-            val idView: TextView = view.id_text
-            val contentView: TextView = view.content
-        }
+        recyclerView.adapter = OrganizatorItemRecyclerViewAdapter(this, DummyContent.ITEMS, twoPane)
     }
 }
--- app/src/main/java/com/rydvi/clean_vrn/ui/organizators/OrganizatorsFragment.kt
+++ app/src/main/java/com/rydvi/clean_vrn/ui/organizators/OrganizatorsFragment.kt
@@ -8,11 +8,14 @@ import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProviders
+import androidx.recyclerview.widget.RecyclerView
 import com.rydvi.clean_vrn.R
+import com.rydvi.clean_vrn.ui.organizators.dummy.DummyContent
 
 class OrganizatorsFragment : Fragment() {
 
     private lateinit var galleryViewModel: OrganizatorsViewModel
+    private var twoPane: Boolean = false
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -22,10 +25,16 @@ class OrganizatorsFragment : Fragment() {
         galleryViewModel =
             ViewModelProviders.of(this).get(OrganizatorsViewModel::class.java)
         val root = inflater.inflate(R.layout.fragment_gallery, container, false)
-        val textView: TextView = root.findViewById(R.id.text_gallery)
-        galleryViewModel.text.observe(this, Observer {
-            textView.text = it
-        })
+//        val textView: TextView = root.findViewById(R.id.text_gallery)
+//        galleryViewModel.text.observe(this, Observer {
+//            textView.text = it
+//        })
+        val organzator_list = root.findViewById<RecyclerView>(R.id.organizator_list)
+        setupRecyclerView(organzator_list)
         return root
+    }
+
+    private fun setupRecyclerView(recyclerView: RecyclerView) {
+        recyclerView.adapter = OrganizatorItemRecyclerViewAdapter(activity!!, DummyContent.ITEMS, twoPane)
     }
 }
