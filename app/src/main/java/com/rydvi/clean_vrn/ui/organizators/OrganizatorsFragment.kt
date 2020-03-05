a7cd65d985ae62505db33536dc34179fc85eef11
RyDVi <dmitrii.kardan.rch@mail.ru>
2020-03-05 16:57:49 +0300

    implemented refresh organizators list

--- app/src/main/java/com/rydvi/clean_vrn/ui/organizators/OrganizatorsFragment.kt
+++ app/src/main/java/com/rydvi/clean_vrn/ui/organizators/OrganizatorsFragment.kt
@@ -8,13 +8,16 @@ import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProviders
 import androidx.recyclerview.widget.RecyclerView
+import androidx.swiperefreshlayout.widget.SwipeRefreshLayout
 import com.rydvi.clean_vrn.R
 import com.rydvi.clean_vrn.api.Organizator
+import com.rydvi.clean_vrn.ui.games.GamesViewModel
 
 class OrganizatorsFragment : Fragment() {
 
     private lateinit var organizatorsViewModel: OrganizatorsViewModel
     private var twoPane: Boolean = false
+    private lateinit var swipeRefreshOrganizators:SwipeRefreshLayout
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -28,11 +31,22 @@ class OrganizatorsFragment : Fragment() {
             val organzator_list = root.findViewById<RecyclerView>(R.id.organizator_list)
             setupRecyclerView(organzator_list, it)
         })
+
+        swipeRefreshOrganizators = root.findViewById(R.id.swipeRefreshOrganizators)
+        swipeRefreshOrganizators.setOnRefreshListener { onRefreshOrganizatorsRecyclerView() }
+
         return root
     }
 
     private fun setupRecyclerView(recyclerView: RecyclerView, organizators: Array<Organizator>) {
         recyclerView.adapter =
             OrganizatorItemRecyclerViewAdapter(activity!!, organizators.toList(), twoPane)
+    }
+
+    private fun onRefreshOrganizatorsRecyclerView(){
+        organizatorsViewModel = ViewModelProviders.of(this).get(OrganizatorsViewModel::class.java)
+        organizatorsViewModel.refreshOrganizators()?.observe(this, Observer {
+            swipeRefreshOrganizators.isRefreshing = false
+        })
     }
 }
